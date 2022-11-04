ade19aaff37e93b41cbec127bfe0388cf19b80db
Chen Ler <lerx98@gmail.com>
2022-11-04 16:35:47 +0800

    .env for frontend

--- frontend/src/services/HistoryService.js
+++ frontend/src/services/HistoryService.js
@@ -1,6 +1,12 @@
-import { HISTORY_SERVICE_ENDPOINT } from "../constants";
-
 const axios = require("axios");
+
+let HISTORY_SERVICE_ENDPOINT = ''
+
+if (process.env.NODE_ENV === "production") {
+    HISTORY_SERVICE_ENDPOINT = process.env.REACT_APP_HISTORY_SERVICE_CLOUD_ENDPOINT;
+} else {
+    HISTORY_SERVICE_ENDPOINT = process.env.REACT_APP_HISTORY_SERVICE_LOCAL_ENDPOINT;
+}
 
 export async function addQuestionToHistory(user, question) {
     const res = await axios.post(HISTORY_SERVICE_ENDPOINT + `${user}`, question);
--- frontend/src/services/QuestionService.js
+++ frontend/src/services/QuestionService.js
@@ -1,6 +1,12 @@
-import { QUESTION_SERVICE_ENDPOINT } from "../constants";
-
 const axios = require("axios");
+
+let QUESTION_SERVICE_ENDPOINT = ''
+
+if (process.env.NODE_ENV === "production") {
+    QUESTION_SERVICE_ENDPOINT = process.env.REACT_APP_QUESTION_SERVICE_CLOUD_ENDPOINT;
+} else {
+    QUESTION_SERVICE_ENDPOINT = process.env.REACT_APP_QUESTION_SERVICE_LOCAL_ENDPOINT;
+}
 
 export async function getQuestion(room_id, difficulty) {
     const res = await axios.get(QUESTION_SERVICE_ENDPOINT + `/difficulty/${difficulty}`, { params: { room: room_id } });
