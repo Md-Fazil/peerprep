67af801fed5d892cfcfbd1770b4993d811598b6c
Md-Fazil <fuzyll10598@gmail.com>
2022-11-06 17:17:16 +0800

    Remove console log

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -3,15 +3,12 @@ const axios = require("axios");
 let USER_SERVICE_ENDPOINT = ''
 
 if (process.env.REACT_APP_NODE_ENV === "production") {
-    console.log("prod")
     USER_SERVICE_ENDPOINT = process.env.REACT_APP_USER_SERVICE_CLOUD_ENDPOINT;
-    console.log(USER_SERVICE_ENDPOINT)
 } else {
     USER_SERVICE_ENDPOINT = process.env.REACT_APP_USER_SERVICE_LOCAL_ENDPOINT;
 }
 
 export async function createUser(data) {
-    console.log(USER_SERVICE_ENDPOINT)
     const res = await axios.post(USER_SERVICE_ENDPOINT + `/`, data);
     console.log(res);
     return res;
