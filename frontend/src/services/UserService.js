1279d39de1ebeacd37028001ca1b6a1aaecd9b92
Md-Fazil <fuzyll10598@gmail.com>
2022-11-06 17:15:31 +0800

    Update environment variable format

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -2,19 +2,23 @@ const axios = require("axios");
 
 let USER_SERVICE_ENDPOINT = ''
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.REACT_APP_NODE_ENV === "production") {
+    console.log("prod")
     USER_SERVICE_ENDPOINT = process.env.REACT_APP_USER_SERVICE_CLOUD_ENDPOINT;
+    console.log(USER_SERVICE_ENDPOINT)
 } else {
     USER_SERVICE_ENDPOINT = process.env.REACT_APP_USER_SERVICE_LOCAL_ENDPOINT;
 }
 
 export async function createUser(data) {
+    console.log(USER_SERVICE_ENDPOINT)
     const res = await axios.post(USER_SERVICE_ENDPOINT + `/`, data);
     console.log(res);
     return res;
 }
 
 export async function loginUser(data) {
+    console.log(USER_SERVICE_ENDPOINT)
     const res = await axios.post(USER_SERVICE_ENDPOINT + `/login`, data);
     console.log(res);
     return res;
